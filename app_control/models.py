e4d4b3430d92450f6f5371d4b6a07806b608016d
John Diginee <Johndbizz@gmail.com>
2023-03-22 16:31:06 +0100

    Add model for inventory group

--- app_control/models.py
+++ app_control/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from user_control.models import CustomUser
+from user_control.views import add_user_activity
 
-# Create your models here.
+class InventoryGroup(models.Model):
+    """Class for creating inventory group."""
+    created_by = models.ForeignKey(
+        CustomUser, null=True, related_name="inventory_groups", 
+        on_delete=models.SET_NULL
+    )
+    name = models.CharField(max_length=100, unique=True)
+    belongs_to = models.ForeignKey(
+        'self', null=True, on_delete=models.SET_NULL, related_name="group_relations"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created_at")
+    
+    def __init__(self, *args, **kwargs):
+        sueper().__init__(*args, **kwargs)
+        self.old_name = self.name
+
+    def save(self, *args, **kwargs):
+        action = f"added new group - '{self.name}'"
+        if self.pk is not None:
+            action = f"updated group from - '{self.old_name}' to '{self.name}'"
+        super().save(*args, **kwargs)
+        add_user_activity(self.created_by, action=action)
+    
+    def delete(self, *args, **kwargs):
+        created_by = self.created_by
+        action = f"deleted group - '{self.name}'"
+        super().delete(*args, **kwargs)
+        add_user_activity(created_by, action=action)
+
+    def __str__(self):
+        return self.name
