a70d6580121e6c37c2db170692aba8d143540cc9
John Diginee <Johndbizz@gmail.com>
2023-03-22 12:21:31 +0100

    Initial commit

---
+++ user_control/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin, BaseUserManager
+)
+
+# Ceating roles
+Roles = (("admin", "admin"), ("creator", "creator"), ("sale", "sale"))
+
+class CustomUserManager(BaseUserManager):
+    """Class for creating custom user manager."""
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Creating superuser."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        """Validating fields."""
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        """Creating fields."""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """Class for creating custom user."""
+    fullname = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=8, choices=Roles)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(null=True)
+
+    USERNAME_FIELD = "email"
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+    
+    class Meta:
+        ordering = ("created_at", )
