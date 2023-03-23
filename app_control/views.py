7ee67cec6f00370f0c1a4675e369f2d2217f1757
John Diginee <Johndbizz@gmail.com>
2023-03-23 15:35:42 +0100

    Finish implementing search and pagination

--- app_control/views.py
+++ app_control/views.py
@@ -4,6 +4,8 @@ from .serializer import (
 )
 from rest_framework.response import Response
 from inventoryx_api.custom_methods import IsAuthenticationCustom
+from inventoryx_api.utils import CustomPagination, get_query
+from django.db.models import Count
 
 
 class InventoryView(ModelViewSet):
@@ -11,6 +13,28 @@ class InventoryView(ModelViewSet):
     queryset = Inventory.objects.select_related("group", "created_by")
     serializer_class = InventorySerializer
     permission_classes = (IsAuthenticationCustom,)
+    pagination_class = CustomPagination
+
+    def get_query(self):
+        """Implementing search."""
+        if self.request.method.lower() != "get":
+            return self.queryset
+        
+        data = self.request.query_params.dict()
+        data.pop("page")
+        keyword = data.pop("keyword", None)
+
+        results = self.queryset(**data)
+
+        if keyword:
+            search_fields = (
+                "code", "created_by__fullname", "created_by__email", 
+                "group__name", "name"
+            )
+            query = get_query(keyword, search_fields)
+            results = results.filter(query)
+
+        return results
 
     def create(self, request, *args, **kwargs):
         """Over ride data"""
@@ -23,6 +47,31 @@ class InventoryGroupView(ModelViewSet):
         "belongs_to", "created_by").prefetch_related("Inventories")
     serializer_class = InventoryGroupSerializer
     permission_classes = (IsAuthenticationCustom,)
+    pagination_class = CustomPagination
+
+    def get_query(self):
+        """Implementing search."""
+        if self.request.method.lower() != "get":
+            return self.queryset
+        
+        data = self.request.query_params.dict()
+        data.pop("page")
+        keyword = data.pop("keyword", None)
+
+        results = self.queryset(**data)
+
+        if keyword:
+            search_fields = (
+                "created_by__fullname", "created_by__email", "name"
+            )
+            query = get_query(keyword, search_fields)
+            results = results.filter(query)
+
+
+        return results.annotate(
+            total_items = Count('inventories')
+        )
+
 
     def create(self, request, *args, **kwargs):
         """Over ride data"""
--- inventoryx_api/utils.py
+++ inventoryx_api/utils.py
@@ -3,6 +3,9 @@ from datetime import datetime, timedelta
 from django.conf import settings
 from user_control.models import CustomUser
 from rest_framework.pagination import PageNumberPagination
+import re
+from django.db.models import Q
+
 
 def get_access_token(payload, days):
     """Define get access token"""
@@ -37,3 +40,27 @@ def decodeJWT(bearer):
 class CustomPagination(PageNumberPagination):
     """Class for pagination."""
     page_size=20
+
+
+def normalize_query(query_string, findterms=re.compile(r'"([^"]+)"|(\S+)').findall, normspace=re.compile(r'\s{2,}').sub):
+    """Regular expression for search keywords."""
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+
+
+def get_query(query_string, search_fields):
+    """Class for search."""
+    query = None  # Query to search for every search term
+    terms = normalize_query(query_string)
+    for term in terms:
+        or_query = None  # Query to search for a given term in each field
+        for field_name in search_fields:
+            q = Q(**{"%s__icontains" % field_name: term})
+            if or_query is None:
+                or_query = q
+            else:
+                or_query = or_query | q
+        if query is None:
+            query = or_query
+        else:
+            query = query & or_query
+        return query
